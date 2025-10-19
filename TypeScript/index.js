0fc0200616c2aa1da9dbc34e5f2ff37aece0a329
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2025-10-19 22:26:30 +0530

    Added React files

--- TypeScript/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Basic Types
-let age = 19;
-let course = "AI - ML";
-let isEnrolled = true;
-// Arrays
-let students = ["John", "Mary", "Jane"];
-let scores = [90, 80, 70];
-let marks = [[90, 100, 80], [70, 90, 50], [80, 90, 100]];
