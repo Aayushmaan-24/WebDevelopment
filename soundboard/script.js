b18e8bcdfd4cf2c72dca43ff42d9a41b64a6e859
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2025-10-26 00:57:52 +0530

    soundboard

---
+++ soundboard/script.js
@@ -0,0 +1,24 @@
+document.getElementById("s1").addEventListener("click", function() {
+    let audio = new Audio("sounds/sound1.mp3");
+    audio.play();
+})
+
+document.getElementById("s2").addEventListener("click", function() {
+    let audio = new Audio("sounds/sound2.mp3");
+    audio.play();
+})
+
+document.getElementById("s3").addEventListener("click", function() {
+    let audio = new Audio("sounds/sound3.mp3");
+    audio.play();
+})
+
+document.getElementById("s4").addEventListener("click", function() {
+    let audio = new Audio("sounds/sound4.mp3");
+    audio.play();
+})
+
+document.getElementById("s5").addEventListener("click", function() {
+    let audio = new Audio("sounds/sound5.mp3");
+    audio.play();
+})
