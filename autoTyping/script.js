c018d9b9c4634d95236906e166f779a8a0ff6918
Aayushmaan <aayushmaan.chakraborty@gmail.com>
2025-10-30 20:16:50 +0530

    Auto typing

---
+++ autoTyping/script.js
@@ -0,0 +1,9 @@
+const typed = new Typed('.auto-type', {
+    strings: ['Developer', 'Designer', 'Freelancer', 'Blogger'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+    backDelay: 1500,
+    cursorChar: '>',
+    smarkBackspace: true,
+})
